90400c47a51d9388987a59304ac43c20ba8a150c
yu-gure <pankotrash@Yu-gure.local>
2026-04-22 12:23:32 -0500

    Observer: created logic for working with Gson and converting from/to map to get ID strings

--- src/main/java/iron/model/DataManager.java
+++ src/main/java/iron/model/DataManager.java
@@ -1,8 +1,18 @@
 package iron.model;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataManager {
     private String folderPath;
-    
+    private final Gson gson = new Gson();
+
     public DataManager(String folderPath) {
         this.folderPath = folderPath;
     }
@@ -11,15 +21,37 @@ public class DataManager {
     public void setFolderPath(String folderPath) { this.folderPath = folderPath; }
     
     public void saveData(String fileName, String jsonString) {
-        // TODO: Implement logic
+        try {
+            String json = gson.toJson(jsonString);
+            Files.write(Paths.get(this.folderPath + fileName + ".json"), json.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-    public String loadData(String fileName) {
-        // TODO: Implement logic
-        return null;
-    }
-    
-    public void updateRecord(String fileName, String id, String newData) {
-        // TODO: Implement logic
+    //put object into map then Gson converts the whole map to the Json
+    public String loadData(String fileName, String id) throws IOException {
+       try {
+           String json = Files.readString(Path.of(folderPath + "/" + fileName));
+           Map<String, String> dataMap = gson.fromJson(json, HashMap.class);
+           return dataMap.get(id);
+       }
+       catch(IOException e) {
+           e.printStackTrace();
+           return null;
+       }
+       }
+
+    //reads the file then GSON converts JSON back into the map and gets the Id then returns a string
+    public void updateRecord(String fileName, String id, String newData) throws IOException {
+        try {
+            String json = Files.readString(Path.of(folderPath + "/" + fileName));
+            String updated = json.replace(id, newData);
+            Files.writeString(Path.of(folderPath + "/" + fileName), updated);
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
